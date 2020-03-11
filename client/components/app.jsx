f762ab4beea4b5dc86426663c28db8c30326a574
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-11 00:34:46 +0000

    Defined ProductList component and imported into App. Moved imported ProductListItem component into ProductList component.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './header';
-import ProductListItem from './product-list-item';
+import ProductList from './product-list';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,7 +14,7 @@ export default class App extends React.Component {
     return (
       <div>
         <Header />
-        <ProductListItem />
+        <ProductList/>
       </div>
     );
   }
---
+++ client/components/product-list.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ProductListItem from './product-list-item';
+
+export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  render() {
+    return (
+      <ProductListItem/>
+    );
+  }
+}
