1d6b7192ad83de49dca466733e4b5c8ae0a7232d
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-11 01:59:11 +0000

    Changed ProductListItem from class component to function component. Products now render.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
-export default class ProductListItem extends React.Component {
+export default function ProductListItem(props) {
+  const productImage = props.productImage;
+  const productName = props.productName;
+  const productPrice = props.productPrice;
+  const productDescription = props.productDescription;
 
-  render(props) {
-    const productImage = props.productImage;
-    const productName = props.productName;
-    const productPrice = props.productPrice;
-    const productDescription = props.productDescription;
-
-    return (
-      <div className="card">
-        <img className="card-img-top" src={productImage} alt="shake-weight"/>
-        <div className="card-body">
-          <h5 className="card-title">{productName}</h5>
-          <p>{productPrice}</p>
-          <p className="card-text">{productDescription}</p>
-        </div>
+  return (
+    <div className="card">
+      <img className="card-img-top" src={productImage} alt="shake-weight"/>
+      <div className="card-body">
+        <h5 className="card-title">{productName}</h5>
+        <p>{productPrice}</p>
+        <p className="card-text">{productDescription}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
