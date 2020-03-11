c04b97fb5af9ccf925135d3242c260ea5a5d1deb
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-11 20:01:03 +0000

    Added componentDidMount() method that calls newly defined getProductDetails() method. Bound getProductDetails().

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -5,10 +5,21 @@ export default class ProductDetails extends React.Component {
     super(props);
     this.state = {
       product: null
-    };
+    }
+    this.getProductDetails = this.getProductDetails.bind(this);
   }
 
-  render() {
+  componentDidMount() {
+   this.getProductDetails();
+  }
+
+  getProductDetails(productId) {
+    fetch(`/api/products/${productId}`)
+      .then(result => result.status(200).json(result.rows[0]))
+      .catch(err => console.err(err));
+  }
+
+  render () {
     return (
       null
     );
