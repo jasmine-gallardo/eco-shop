09e046c498f1a9065882bc44bd6c77cd84f6f102
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-11 19:33:47 +0000

    Added ProductDetails component with initial state and initial render method.

---
+++ client/components/product-details.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class ProductDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: null
+    };
+  }
+
+  render() {
+    return (
+      null
+    );
+  }
+}
