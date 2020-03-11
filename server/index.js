ef9aaec0ba826597f00109d588f658f00db7dcf1
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-11 18:28:04 +0000

    Added db.query() for single product details and error handling.

--- server/index.js
+++ server/index.js
@@ -34,13 +34,22 @@ app.get('/api/products', (req, res, next) => {
 });
 
 app.get('/api/products/:productId', (req, res, next) => {
-  const productId = req.params.gradeId;
+  const productId = req.params.productId;
   const sql = `
     select *
       from "products"
     where "productId" = $1
   `;
   const values = [productId];
+
+  db.query(sql, values)
+    .then(result => {
+      if (!result.rows.productId) {
+        next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
+      }
+      res.status(200).json(result.rows[0]);
+    })
+    .catch(err => next(err));
 });
 
 app.use('/api', (req, res, next) => {
