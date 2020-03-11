346e3f23fa3a5de2cbfa190394bf97d91b08d4c2
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-03-11 19:29:03 +0000

    Edited view property value on state object to be an object with  name and params properties.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: true
+      view: { name: 'catalog', params: {} }
     };
   }
 
