ca7d4181ea20f2819c474ce9ac8fec6f2cebf6ef
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-07 20:58:50 +0000

    Adjusted modal component bootstrap class.

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export default function DisclaimerModal(props) {
   return (
-    <div className="modal-bg">
-      <div className="text-center">
-        <p className="display-3">This site is for demonstrational purposes only</p>
-        <p className="h2">By clicking `&quot;`I Understand`&quot;`, I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
-        <button className="btn btn-warning">I Understand</button>
+    <div className="modal-bg d-flex">
+      <div className="container m-auto text-center">
+        <p className="display-4">This site is for demonstrational purposes only</p>
+        <p className="lead">By clicking &quot;I Understand&quot;, I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
+        <button className="btn btn-danger">I Understand</button>
       </div>
     </div>
   );
