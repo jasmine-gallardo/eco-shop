a3f9ee226d4103d09ddcb35724e8c3d1f87addbc
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-07 21:10:42 +0000

    Added onClick to change view to catalog.

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -6,7 +6,7 @@ export default function DisclaimerModal(props) {
       <div className="container m-auto text-center">
         <p className="display-4">This site is for demonstrational purposes only</p>
         <p className="lead">By clicking &quot;I Understand&quot;, I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
-        <button className="btn btn-danger">I Understand</button>
+        <button onClick={() => props.setView('catalog', {})} className="btn btn-danger">I Understand</button>
       </div>
     </div>
   );
