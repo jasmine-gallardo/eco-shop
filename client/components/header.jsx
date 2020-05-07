4c1acb32855e4bece0e306944c4d5d906f36b19f
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-07 21:10:04 +0000

    Added onClick to site name in header to change view back to catalog.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,14 +7,14 @@ export default class Header extends React.Component {
     return (
       <div className="container header-container">
         <header className="row p-2 bg-dark text-white">
-          <div className="wicked-sales-text col-md-9 col-sm-9 d-flex">
+          <div className="wicked-sales-text col-md-9 col-sm-9 d-flex" onClick={() => setView('catalog', {})}>
             <i className="fas fa-leaf fa-2x d-inline-block m-2"></i>
-            <h2 className="d-inline-block">Eco Shop</h2>
+            <h2 className="clickable-header d-inline-block">Eco Shop</h2>
           </div>
           <div className="cart-items col-md-3 col-sm-3 d-flex justify-content-center align-items-center"
             onClick={() => setView('cart', {})}>
             <p className="m-1">{`${cartItemCount} items`}</p>
-            <i className="fas fa-shopping-cart fa-2x"></i>
+            <i className="clickable-header fas fa-shopping-cart fa-2x"></i>
           </div>
         </header>
       </div>
