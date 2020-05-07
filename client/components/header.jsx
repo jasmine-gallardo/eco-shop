6c42de6b38e008317b2df123db8e8b42d3bd5bda
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-07 19:39:48 +0000

    Changed title and icon to match customized theme.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -8,8 +8,8 @@ export default class Header extends React.Component {
       <div className="container header-container">
         <header className="row p-2 bg-dark text-white">
           <div className="wicked-sales-text col-md-9 col-sm-9 d-flex">
-            <i className="fas fa-dollar-sign fa-2x d-inline-block m-2"></i>
-            <h2 className="d-inline-block">Wicked Sales</h2>
+            <i className="fas fa-leaf fa-2x d-inline-block m-2"></i>
+            <h2 className="d-inline-block">Eco Shop</h2>
           </div>
           <div className="cart-items col-md-3 col-sm-3 d-flex justify-content-center align-items-center"
             onClick={() => setView('cart', {})}>
