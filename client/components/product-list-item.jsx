393142052ea11100f8603d37936cd05f10f84d5b
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-08 01:40:37 +0000

    Adjusted grid styling for responsiveness.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -9,9 +9,9 @@ export default function ProductListItem(props) {
   const setViewProp = props.setViewProp;
 
   return (
-    <div className="item card col-md-3 m-2 col-sm-5 col-9 shadow-sm" onClick={() => setViewProp('details', { productId })}>
-      <img className="card-img-top my-4" src={productImage}/>
-      <div className="card-body p-2 mb-5">
+    <div className="item card col-md-3 m-2 col-sm-5 col-9 shadow-sm pb-md-5" onClick={() => setViewProp('details', { productId })}>
+      <img className="card-img-top mt-lg-4 mt-md-2 mt-sm-3 mt-5 mb-2 h-50" src={productImage}/>
+      <div className="card-body p-2 mt-lg-3 mt-md-3 mt-4">
         <h5 className="card-title">{productName}</h5>
         <p className="text-muted">{productPrice}</p>
         <p className="card-text">{productDescription}</p>
