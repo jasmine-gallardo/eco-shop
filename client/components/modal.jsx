7204a0a590660722d31be7c1accc7e6b955d3eb9
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-12 20:18:59 +0000

    Adjusted text size and bootstrap height class.

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function DisclaimerModal(props) {
   return (
-    <div className="modal-bg d-flex">
-      <div className="container m-auto text-center">
-        <p className="display-4">This site is for demonstrational purposes only</p>
-        <p className="lead">By clicking &quot;I Understand&quot;, I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
+    <div className="modal-bg vh-100 d-flex align-items-center">
+      <div className="m-auto text-center col-11">
+        <h2 className="mb-3">This site is for demonstrational purposes only.</h2>
+        <p className="lead">By clicking &quot;I Understand&quot; I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
         <button onClick={() => props.setView('catalog', {})} className="btn btn-danger">I Understand</button>
       </div>
     </div>
