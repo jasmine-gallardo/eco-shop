d9652fc12c3b037010d0b258cf94c7c433beaf5d
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-05-12 19:30:10 +0000

    Adjusted margin and padding.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 export default function CartSummaryItem(props) {
   return (
-    <div className="card mt-4 mb-4 cart-summary-item col-11 m-auto">
+    <div className="card card-body cart-summary-item my-4">
       <div className="row no-gutters">
-        <div className="col-lg-3 col-md-4 p-0">
+        <div className="col-lg-4 col-md-4 p-0 m-auto">
           <img className="card-img cart-item-img h-100 py-3" src={props.image} />
         </div>
-        <div className="col-md-8 d-flex m-auto">
-          <div className="card-body m-auto">
+        <div className="col-lg-7 col-md-7 d-flex">
+          <div className="card-body m-auto p-4">
             <h3 className="card-title">{props.name}</h3>
             <p className="card-text text-muted">{`$${(props.price / 100).toFixed(2)}`}</p>
             <p className="card-text">{props.shortDescription}</p>
