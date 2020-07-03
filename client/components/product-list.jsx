d4dcd0ab7625543e6d01901964b2787935b2718c
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-03 23:34:30 +0000

    Deleted unnecessary div.

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -25,17 +25,15 @@ export default class ProductList extends React.Component {
     const setViewProp = this.props.setViewProp;
     const productsListArray = this.state.products;
     return (
-      <div>
-        <div className="hero-container row py-5 d-flex align-items-center">
-          <div className="hero-image ml-auto text-center row col-7"></div>
-          <div className="col-3 ml-3 mr-auto d-flex">
-            <div className="position-helper">
-              <i className="fas fa-leaf fa-7x mb-4"></i>
-              <div className="header-text">
-                <h4 className="display-4 text-dark typeface-1 font-weight-bold">Seasonal Sale</h4>
-                <p>Claim our best products of the season </p>
-                <a href="#scroll-catalog-view" className="btn btn-block btn-dark">Shop now</a>
-              </div>
+      <div className="hero-container row py-5 align-items-center">
+        <div className="hero-image ml-auto text-center row col-12 col-md-7"></div>
+        <div className="header-col-2 col-12 col-md-3 ml-md-3 mr-auto">
+          <div className="position-helper">
+            <i className="fas fa-leaf fa-7x mb-4"></i>
+            <div className="header-text">
+              <h4 className="text-dark typeface-1 font-weight-bold">Seasonal Sale</h4>
+              <p>Claim our best products of the season </p>
+              <a href="#scroll-catalog-view" className="btn btn-block btn-dark">Shop now</a>
             </div>
           </div>
         </div>
