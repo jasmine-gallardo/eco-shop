5147a4d48e56961905c99a961ce64faf31784069
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-03 22:38:26 +0000

    Changed to lighter theme and changed typeface.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,15 +6,17 @@ export default class Header extends React.Component {
     const setView = this.props.setView;
     return (
       <div className="header-container">
-        <header className="row fixed-top py-3 px-5 bg-dark text-white shadow-sm">
-          <div className="wicked-sales-text ml-md-5 ml-sm-2 d-flex" onClick={() => setView('catalog', {})}>
-            <i className="fas fa-leaf fa-1x m-2"></i>
-            <p className="clickable-header lead mb-0">Eco Shop</p>
+        <header className="row py-3 px-3 bg-light text-dark">
+          <div className="clickable cart-items col-4 p-0 col-md-2 d-flex align-items-center justify-content-center">
+            {/* <i class="fas fa-bars fa-1x"></i> */}
           </div>
-          <div className="cart-items ml-auto mr-md-5 mr-sm-2 d-flex align-items-center"
+          <div className="brand-name col-4 justify-content-center m-auto d-flex clickable center" onClick={() => setView('catalog', {})}>
+            <p className="mb-0 typeface-1 font-weight-bold">Eco Shop</p>
+          </div>
+          <div className="clickable cart-items col-4 p-0 col-md-2 d-flex align-items-center justify-content-center"
             onClick={() => setView('cart', {})}>
-            <p className="m-1">{`${cartItemCount} items`}</p>
-            <i className="clickable-header fas fa-shopping-cart fa-1x"></i>
+            <p className="m-1 typeface-1 cart-text">{`${cartItemCount} items`}</p>
+            <i className="fas fa-shopping-cart fa-1x"></i>
           </div>
         </header>
       </div>
