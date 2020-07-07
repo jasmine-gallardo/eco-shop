ff7128ebd5a53eb25a18ca5edf101cd7f99a8dd8
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-07 22:40:46 +0000

    Added toggleButton method to modal that adds 'display: none' to modal style.

--- client/components/modal.jsx
+++ client/components/modal.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
-export default function DisclaimerModal(props) {
-  return (
-    <div className="modal-bg vh-100 d-flex align-items-center">
-      <div className="m-auto text-center col-11">
-        <h2 className="mb-3">This site is for demonstrational purposes only.</h2>
-        <p className="lead">By clicking &quot;I Understand&quot; I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
-        <button onClick={() => props.setView('catalog', {})} className="btn btn-danger">I Understand</button>
+export default class DisclaimerModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false
+    };
+    this.toggleButton = this.toggleButton.bind(this);
+  }
+
+  toggleButton() {
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    if (this.state.clicked === true) {
+      const modalContainer = document.querySelector('.modal-container');
+      modalContainer.classList.add('no-display');
+    }
+    return (
+      <div className="modal-bg align-items-center">
+        <div className="modal-container col-11 col-lg-6">
+          <h2 className="mb-3">This site is for demonstrational purposes only.</h2>
+          <p className="lead mb-4">By clicking &quot;I Understand&quot; I acknowledge that I cannot make real purchases and should not use my personal or financial information including my real name, address, or credit card information.</p>
+          <div className="text-center">
+            <button onClick={() => this.toggleButton()} className="btn btn-danger">I Understand</button>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
