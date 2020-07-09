b12455fd591901536a384f2a16a7ee8cd0088f6f
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-09 23:11:59 +0000

    Moved closeCart() to App.jsx to access method from header.

--- client/components/preview-cart-summary.jsx
+++ client/components/preview-cart-summary.jsx
@@ -2,25 +2,12 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 export default class PreviewCartSummary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.closeCart = this.closeCart.bind(this);
-  }
-
-  closeCart() {
-    const cartPreview = document.querySelector('.cart-summary-drawer');
-    cartPreview.classList.add('no-display');
-    cartPreview.classList.remove('shadow-lg');
-    const cartModal = document.querySelector('.cart-click-away');
-    cartModal.classList.add('d-none');
-  }
-
   render() {
     if (this.props.cart.length === 0) {
       return (
         <div className="cart-summary-drawer text-center row justify-content-center no-display">
           <div className="cart-drawer-2 border-bottom row d-flex col-12">
-            <i onClick={() => this.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
+            <i onClick={() => this.props.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
             <p className="col-11 text-right">{this.props.cart.length} items </p>
           </div>
           <div className="container text-center">
@@ -35,7 +22,7 @@ export default class PreviewCartSummary extends React.Component {
     return (
       <div className="cart-summary-drawer text-center row justify-content-center no-display">
         <div className="cart-drawer-2 border-bottom row d-flex col-12 shadow-sm">
-          <i onClick={() => this.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
+          <i onClick={() => this.props.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
           <p className="col-11 text-right typeface-1 font-weight-bold">{this.props.cart.length} items </p>
         </div>
         <div className="cart-drawer-content">
@@ -57,7 +44,7 @@ export default class PreviewCartSummary extends React.Component {
           <div className="checkout-section px-4 py-3">
             <p className="font-weight-bold">Estimated Total</p>
             <p className="font-weight-bold pr-1">${itemTotal}</p>
-            <button onClick={() => { this.closeCart(); this.props.setView('checkout', {}); }} className="btn btn-block btn-dark mb-3">Checkout</button>
+            <button onClick={() => { this.props.closeCart(); this.props.setView('checkout', {}); }} className="btn btn-block btn-dark mb-3">Checkout</button>
             <p className="text-left disclaimer-text">Reminder: This site is not a real shop, it&apos;s a project. Do not use your real information when &quot;checking out&quot;.</p>
           </div>
         </div>
