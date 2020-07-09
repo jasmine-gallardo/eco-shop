716f29a4b411782bd8703de679d1485cd954f6c7
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-09 16:14:37 +0000

    Added openCartDrawer() to cart icon and passed setView() as prop to cart drawer.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -10,7 +10,7 @@ export default class Header extends React.Component {
       <div>
         <DisclaimerModal />
         <div className="header-container">
-          <PreviewCartSummary cart={this.props.cart}/>
+          <PreviewCartSummary cart={this.props.cart} setView={this.props.setView}/>
           <header className="row py-3 px-3 bg-light text-dark ">
             <div className="clickable cart-items col-3 p-0 col-md-2 d-flex align-items-center justify-content-center">
               {/* <i class="fas fa-bars fa-1x"></i> */}
@@ -19,7 +19,7 @@ export default class Header extends React.Component {
               <p className="mb-0 typeface-1 font-weight-bold eco-shop-text">Eco Shop</p>
             </div>
             <div className="clickable cart-items col-3 pl-0 pr-2 col-md-2 d-flex align-items-center justify-content-center"
-              onClick={() => setView('cart', {})}>
+              onClick={() => this.props.openCartDrawer()}>
               <p className="m-1 typeface-1 cart-text">{`${cartItemCount} items`}</p>
               <i className="fas fa-shopping-cart fa-1x"></i>
             </div>
