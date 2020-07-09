9249871a2665a3ee563e680c9b8460c3a8f762a1
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-09 04:31:58 +0000

    Imported CartSummaryItem component and consitional for when no items are in cart.

--- client/components/preview-cart-summary.jsx
+++ client/components/preview-cart-summary.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CartSummaryItem from './cart-summary-item';
 
 export default class PreviewCartSummary extends React.Component {
   constructor(props) {
@@ -7,19 +8,46 @@ export default class PreviewCartSummary extends React.Component {
   }
 
   closeCart() {
-    const cartPreview = document.querySelector('.item-added');
+    const cartPreview = document.querySelector('.cart-summary-drawer');
     cartPreview.classList.add('no-display');
   }
 
   render() {
-    return (
-      <div className="item-added text-center row shadow-lg ">
-        <div className="item-added-2 border-bottom row d-flex col-12">
-          <i onClick={() => this.closeCart()} className="far fa-times-circle col-1 p-0"></i>
-          <p className="col-11 text-right">{this.props.cartItemCount} items </p>
+    if (this.props.cart.length === 0) {
+      return (
+        <div className="cart-summary-drawer text-center row shadow-lg justify-content-center">
+          <div className="cart-drawer-2 border-bottom row d-flex col-12">
+            <i onClick={() => this.closeCart()} className="far fa-times-circle col-1 p-0"></i>
+            <p className="col-11 text-right">{this.props.cart.length} items </p>
+          </div>
+          <div className="container text-center">
+            <p>There are no items in your cart.</p>
+          </div>
         </div>
-        <div>
-          <p className="mx-auto"> Item added to cart</p>
+      );
+    }
+    return (
+      <div className="cart-summary-drawer text-center row shadow-lg justify-content-center">
+        <div className="cart-drawer-2 border-bottom row d-flex col-12">
+          <i onClick={() => this.closeCart()} className="far fa-times-circle col-1 p-0"></i>
+          <p className="col-11 text-right">{this.props.cart.length} items </p>
+        </div>
+        <div className="cart-drawer-content">
+          <div className="cart-drawer-3">
+            {
+              this.props.cart.map(item => {
+                return (
+                  <CartSummaryItem
+                    key={item.cartItemId}
+                    image={item.image}
+                    name={item.name}
+                    price={item.price}
+                    shortDescription={item.shortDescription}
+                  />
+                );
+              })
+            }
+          </div>
         </div>
       </div>
     );
