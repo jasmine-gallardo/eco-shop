27fd49f222a118a752556f84d3e02d7f799bc7a8
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-09 04:33:49 +0000

    Changed component structure and style to fit in PreviewCartSummary drawer.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 export default function CartSummaryItem(props) {
   return (
-    <div className="card card-body cart-summary-item my-4">
-      <div className="row no-gutters">
-        <div className="col-lg-4 col-md-4 p-0 m-auto">
-          <img className="card-img cart-item-img h-100 py-3" src={props.image} />
+    <div className="cart-summary-item card d-flex">
+      <div className="cart-summary-item-2 border-bottom">
+        <div className="text-left cart-item-text">
+          <p className="mb-0 font-weight-bold">{props.name}</p>
+          <p className="text-muted">{`$${(props.price / 100).toFixed(2)}`}</p>
         </div>
-        <div className="col-lg-7 col-md-7 d-flex">
-          <div className="card-body m-auto p-4">
-            <h3 className="card-title">{props.name}</h3>
-            <p className="card-text text-muted">{`$${(props.price / 100).toFixed(2)}`}</p>
-            <p className="card-text">{props.shortDescription}</p>
-          </div>
+        <div className="p-0">
+          <img className="card-img" src={props.image}/>
+        </div>
+      </div>
+      <div className="">
+        <div className="cart-edit-text row text-center m-0 py-2">
+          <p className="mb-0 col-6 border-right-0 p-0">Edit</p>
+          <p className="mb-0 col-6 border-left p-0">Remove</p>
         </div>
       </div>
     </div>
