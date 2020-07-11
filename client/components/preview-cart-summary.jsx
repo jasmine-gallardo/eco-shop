2e880b04c2826560f5b6a03b295be5c91a9cc9d1
jasmine-gallardo <j.m.gallardo6@gmail.com>
2020-07-11 00:36:57 +0000

    Changed global dom queries for drawer style to be managed by App component state instead.

--- client/components/preview-cart-summary.jsx
+++ client/components/preview-cart-summary.jsx
@@ -3,15 +3,22 @@ import CartSummaryItem from './cart-summary-item';
 
 export default class PreviewCartSummary extends React.Component {
   render() {
+    const cartDisplayStyles = this.props.cartDisplayStyles;
+    const bootstrapDisplay = cartDisplayStyles.bootstrapClass.display;
+    const bootstrapShadow = cartDisplayStyles.bootstrapClass.shadow;
+    const customDisplay = cartDisplayStyles.customClass;
     if (this.props.cart.length === 0) {
       return (
-        <div className="cart-summary-drawer text-center row justify-content-center no-display">
-          <div className="cart-drawer-2 border-bottom row d-flex col-12">
-            <i onClick={() => this.props.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
-            <p className="col-11 text-right">{this.props.cart.length} items </p>
-          </div>
-          <div className="container text-center">
-            <p>There are no items in your cart.</p>
+        <div>
+          <div onClick={() => this.props.closeCart()} className={`cart-click-away ${bootstrapDisplay}`}></div>
+          <div className={`cart-summary-drawer text-center row justify-content-center ${customDisplay} ${bootstrapShadow}`}>
+            <div className="cart-drawer-2 border-bottom row d-flex col-12">
+              <i onClick={() => this.props.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
+              <p className="col-11 text-right">{this.props.cart.length} items </p>
+            </div>
+            <div className="container text-center">
+              <p>There are no items in your cart.</p>
+            </div>
           </div>
         </div>
       );
@@ -20,33 +27,36 @@ export default class PreviewCartSummary extends React.Component {
     const itemTotalReduce = cartArrayCopy.reduce((acc, curr) => acc + curr);
     const itemTotal = (itemTotalReduce / 100).toFixed(2);
     return (
-      <div className="cart-summary-drawer text-center row justify-content-center no-display">
-        <div className="cart-drawer-2 border-bottom row d-flex col-12 shadow-sm">
-          <i onClick={() => this.props.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
-          <p className="col-11 text-right typeface-1 font-weight-bold">{this.props.cart.length} items </p>
-        </div>
-        <div className="cart-drawer-content">
-          <div className="cart-drawer-3 justify-content-center">
-            {
-              this.props.cart.map(item => {
-                return (
-                  <CartSummaryItem
-                    key={item.cartItemId}
-                    image={item.image}
-                    name={item.name}
-                    price={item.price}
-                    shortDescription={item.shortDescription}
-                  />
-                );
-              })
-            }
+      <div>
+        <div onClick={() => this.props.closeCart()} className={`cart-click-away ${bootstrapDisplay}`}></div>
+        <div className={`cart-summary-drawer text-center row justify-content-center ${customDisplay} ${bootstrapShadow}`}>
+          <div className="cart-drawer-2 border-bottom row d-flex col-12 shadow-sm">
+            <i onClick={() => this.props.closeCart()} className="clickable far fa-times-circle col-1 p-0"></i>
+            <p className="col-11 text-right typeface-1 font-weight-bold">{this.props.cart.length} items </p>
           </div>
-        </div>
-        <div className="checkout-section px-4 py-3">
-          <p className="font-weight-bold">Estimated Total</p>
-          <p className="font-weight-bold pr-1">${itemTotal}</p>
-          <button onClick={() => { this.props.closeCart(); this.props.setView('checkout', {}); }} className="btn btn-block btn-dark mb-3">Checkout</button>
-          <p className="text-left disclaimer-text">Reminder: This site is not a real shop, it&apos;s a project. Do not use your real information when &quot;checking out&quot;.</p>
+          <div className="cart-drawer-content">
+            <div className="cart-drawer-3 justify-content-center">
+              {
+                this.props.cart.map(item => {
+                  return (
+                    <CartSummaryItem
+                      key={item.cartItemId}
+                      image={item.image}
+                      name={item.name}
+                      price={item.price}
+                      shortDescription={item.shortDescription}
+                    />
+                  );
+                })
+              }
+            </div>
+          </div>
+          <div className="checkout-section px-4 py-3">
+            <p className="font-weight-bold">Estimated Total</p>
+            <p className="font-weight-bold pr-1">${itemTotal}</p>
+            <button onClick={() => { this.props.closeCart(); this.props.setView('checkout', {}); }} className="btn btn-block btn-dark mb-3">Checkout</button>
+            <p className="text-left disclaimer-text">Reminder: This site is not a real shop, it&apos;s a project. Do not use your real information when &quot;checking out&quot;.</p>
+          </div>
         </div>
       </div>
     );
